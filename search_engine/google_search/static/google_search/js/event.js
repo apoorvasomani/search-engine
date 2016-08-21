12db1f8b60acf724dc9e617f0802c226409bf9c8
apoorva <apoorva@hackerearth.com>
2016-08-21 06:47:35 +0530

    google_search: added youtube player and sticky note

--- search_engine/google_search/static/google_search/js/event.js
+++ search_engine/google_search/static/google_search/js/event.js
@@ -1,5 +1,13 @@
-$('.search-button').click(function() {
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player');
+}
 
+$('.search-button').click(function() {
 	var url = $(this).attr('url');
 	var search_query = $('.search-text').val();
 
@@ -15,4 +23,70 @@ $('.search-button').click(function() {
 			success: function(result) {
 				$('.search-result').html(result);
     }});
+});
+
+$(function() {
+	$(".video-player").draggable({containment: 'body'});
+	$(".video-player").resizable();
+	$(".sticky-note").draggable({containment: 'body'});
+	$(".sticky-note").resizable();
+});
+
+$('.player-move').mousedown(function() {
+	$(this).addClass('fa-hand-grab-o');
+	$(this).removeClass('fa-hand-stop-o');
+});
+
+$('.player-move').mouseup(function() {
+	$(this).removeClass('fa-hand-grab-o');
+	$(this).addClass('fa-hand-stop-o');
+});
+
+$('.player-close').mouseup(function() {
+	$('.video-player').hide();
+	player.pauseVideo();
+});
+
+$('.video-url').on('change', function(){
+	var p = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+	var url = $(this).val();
+	var validity = url.match(p);
+	if (validity)
+	{
+		$('#player').attr('src', convertUrlToEmbedded(url));
+	}
+	else {
+		$('.error-msg').fadeIn(3000, function() {
+			$('.error-msg').html('Not a youtube URL!!');
+			$('.error-msg').fadeOut(3000);
+		});
+	}
+});
+
+$('.video-bar').on('click',function(){
+	if($('.video-player').css('display') == 'none') {
+		$('.video-player').show();
+		player.playVideo();
+	}
+	else {
+		$('.video-player').hide();
+		player.pauseVideo();
+	}
+});
+
+function convertUrlToEmbedded(url) {
+	var pattern1 = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g;
+	var replacement = 'http://www.youtube.com/embed/$1?enablejsapi=1';
+	var url = url.replace(pattern1, replacement);
+
+	return url;
+}
+
+$('.note-bar').on('click',function(){
+	if($('.sticky-note').css('display') == 'none') {
+		$('.sticky-note').show();
+	}
+	else {
+		$('.sticky-note').hide();
+	}
 });
